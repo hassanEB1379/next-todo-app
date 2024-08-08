e3861bf6cdef14680ac586ec2a7b4ed3d7fb2544
hassan <hassanebrahimi560@gmail.com>
2024-08-08 19:47:59 +0330

    init project

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,16 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
+import { ColorSchemeScript } from "@mantine/core";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>My awesome app</title>
+
+        <ColorSchemeScript />
+      </Head>
       <body>
         <Main />
         <NextScript />
